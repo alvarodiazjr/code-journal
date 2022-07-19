cc4cbcbca868aad0ae01ee6dba871c859d9249f9
Alvaro Diaz <alvarodiazjr@yahoo.com>
2022-07-19 13:22:44 -0700

    issue 1 first completion attemp.

--- js/data.js
+++ js/data.js
@@ -6,3 +6,8 @@ var data = {
   editing: null,
   nextEntryId: 1
 };
+
+window.addEventListener('beforeunload', function unload(event){
+  localStorage.getItem('code-journal-local-storage');
+  localStorage.setItem('code-journal-local-storage', JSON.stringify(data));
+});
